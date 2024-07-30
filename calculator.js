82b1836dadecc9d3ae8292b6ce909e8b1f24c5c5
DESKTOP-4375EAV\gusdn <gusdn276@naver.com>
2024-07-30 23:06:27 +0900

    css height조절, js 키보드 입력으로 계산기 조작하는거 완료

--- calculator.js
+++ calculator.js
@@ -15,17 +15,65 @@ const $multiple = document.querySelector("#multiple");
 const $divide = document.querySelector("#divide");
 const $calculate = document.querySelector("#calculate");
 const $clear = document.querySelector("#clear");
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let result;
+let calculationCompleted = false;
 
 document.addEventListener("keydown", (event) => {
   const operators = ["+", "-", "*", "/"];
 
   if(event.key >= "0" && event.key <= "9") {
+    if(calculationCompleted) {
+      calculationCompleted = false;
+    }
     $result.value += event.key;
+    if(firstNumber === "") {
+      firstNumber += event.key;
+    } else if(firstNumber !== "") {
+      secondNumber += event.key;
+    }
   } 
-  if(event.key === "Backspace") {
+  if(event.key === "Backspace" && !calculationCompleted) {
     $result.value = $result.value.slice(0, -1);
   }
   if(operators.includes(event.key)) {
+    if(calculationCompleted) {
+      calculationCompleted = false;
+    }
     $result.value += `${event.key}`;
+    operator = event.key;
   }
+  if(event.key === "Enter" && firstNumber !== result) {
+    const num1 = parseFloat(firstNumber);
+    const num2 = parseFloat(secondNumber);
+    if(operator === "+") {
+      result = num1 + num2;
+    } else if(operator === "-") {
+      result = num1 - num2;
+    } else if(operator === "*") {
+      result = num1 * num2;
+    } else if(operator === "/") {
+      result = num1 / num2;
+    }
+    firstNumber = result;
+    $result.value = result;
+    operator = "";
+    secondNumber = "";
+    result = null;
+    calculationCompleted = true;
+  }
+  if(event.key === "Escape") {
+    $result.value = "";
+    firstNumber = "";
+    operator = "";
+    secondNumber = "";
+    result = "";
+    calculationCompleted = false;
+  }
+});
+
+$one.addEventListener("click", (event) => {
+
 });
